754bfebf9e4ff0c3686c4484e9461cd30b71b978
neocim <frag1337n@gmail.com>
2025-06-01 21:08:59 +0300

    Add basic files

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,6 +1,9 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Api;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddPresentation();
+
+var app = builder.Build();
+app.MapControllers();
 
 app.Run();
