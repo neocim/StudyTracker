2acf9ee97d4153bd936e2822a63a039937662dfa
neocim <frag1337n@gmail.com>
2025-06-12 18:15:10 +0300

    Update db context

--- src/Infrastructure/Database/ApplicationDbContext.cs
+++ src/Infrastructure/Database/ApplicationDbContext.cs
@@ -4,8 +4,17 @@ using Task = Domain.Entities.Task;
 
 namespace Infrastructure.Database;
 
-public class ApplicationDbContext(DbContextOptions options) : DbContext(options)
+public class ApplicationDbContext(DbContextOptions options)
+    : DbContext(options)
 {
     public DbSet<User> Users { get; set; } = null!;
     public DbSet<Task> Tasks { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(
+            typeof(ApplicationDbContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
 }
--- src/Infrastructure/Database/Configuration/Task.cs
+++ src/Infrastructure/Database/Configuration/Task.cs
@@ -1,7 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Task = Domain.Entities.Task;
-using Domain.Entities;
 
 namespace Infrastructure.Database.Configuration;
 
