6d6aa87c10ac356ce20dffb9372b4d9ff5ebac8f
neocim <frag1337n@gmail.com>
2025-06-16 21:23:32 +0300

    Add dependency injetion and init `CQRS`

---
+++ src/Application/Cqrs/Queries/User/CreateNew.cs
@@ -0,0 +1,23 @@
+using Application.Repositories;
+using Entity = Domain.Entities;
+using ErrorOr;
+using MediatR;
+
+namespace Application.Cqrs.Queries.User;
+
+public record CreateNewUserCommand(Entity.User User) : IRequest<ErrorOr<Success>>;
+
+public class CreateNewUserCommandHandler(IUserRepository userRepository)
+    : IRequestHandler<CreateNewUserCommand, ErrorOr<Success>>
+{
+    public async Task<ErrorOr<Success>> Handle(CreateNewUserCommand request,
+        CancellationToken cancellationToken)
+    {
+        if (await userRepository.GetByIdAsync(request.User.Id) is not null)
+            return Error.Conflict($"User with ID `{request.User.Id}` is already exists");
+
+        await userRepository.AddAsync(request.User);
+
+        return Result.Success;
+    }
+}
---
+++ src/Application/DependencyInjection.cs
@@ -0,0 +1,14 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(options =>
+            options.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
+
+        return services;
+    }
+}
