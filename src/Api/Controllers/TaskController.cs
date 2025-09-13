7e53d5c4d7165de9735ba5cf972c154a05ae9dbc
neocim <frag1337n@gmail.com>
2025-09-13 16:37:53 +0300

    Add forgotten `[Authorize]` attribute

--- src/Api/Controllers/TaskController.cs
+++ src/Api/Controllers/TaskController.cs
@@ -5,10 +5,12 @@ using Application.Cqrs.Queries.Task;
 using Application.Dto.Task.ReadModels;
 using AutoMapper;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers;
 
+[Authorize]
 [Route("users/{userId:guid}")]
 public class TasksController(IMediator mediator, IMapper mapper) : ApiController
 {
